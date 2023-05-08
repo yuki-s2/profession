60f4a4b661d7640f8729d342b376434d286b3337
Yuki Honda <dahlia.15s2@gmail.com>
2023-05-08 13:45:44 +0900

    横スライド調整

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
-const listEl = document.querySelector('.side-scroll-list');
+//const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
+//const listEl = document.querySelector('.side-scroll-list');
+//
+//gsap.to(listEl, {
+//  x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
+//  ease: 'none',
+//  scrollTrigger: {
+//    trigger: '.side-scroll',
+//    start: 'top top', // 要素の上端（top）が、ビューポートの上端（top）にきた時
+//    end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
+//    scrub: true,
+//    pin: true,
+//    anticipatePin: 1,
+//    invalidateOnRefresh: true,
+//  },
+//});
 
-gsap.to(listEl, {
-  x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
+
+
+const itemWrapper = document.querySelector('.side-scroll__itemOuter');
+const itemInner = document.querySelector('.side-scroll__itemInner');
+
+gsap.to(itemInner, {
+  x: () => -(itemInner.clientWidth - itemWrapper.clientWidth),
   ease: 'none',
   scrollTrigger: {
-    trigger: '.side-scroll',
-    start: 'top top', // 要素の上端（top）が、ビューポートの上端（top）にきた時
-    end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-    invalidateOnRefresh: true,
+    trigger: '.side-scroll__section', 
+    start: 'top top',
+    end: () => `+=${itemInner.clientWidth - itemWrapper.clientWidth}`,
+   // markers: true,
+    scrub: true, 
+    pin: true,  
+    invalidateOnRefresh: true, 
+    anticipatePin: 1, 
   },
 });
